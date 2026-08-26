5cfabf0b712c368653cf1ce319d04c7255ccfe03
pob <qnas0202@gmail.com>
2026-08-26 19:43:45 +0900

    걸음수

--- board_game.js
+++ board_game.js
@@ -2061,7 +2061,7 @@ function sceneSafeArea(idx) {
 
 function rollPatrolEvents(idx, scene = null) {
   const area = scene?.board?.areas?.[idx] || sceneSafeArea(idx);
-  const maxSteps = Phaser.Math.Between(28 + area.dungeonNo * 2, 40 + area.dungeonNo * 3);
+  const maxSteps = Phaser.Math.Between(56 + area.dungeonNo * 4, 80 + area.dungeonNo * 6);
   const picked = [];
   let step = Phaser.Math.Between(4, 9);
   while (step < maxSteps) {
@@ -2107,7 +2107,7 @@ function startExploreRun(scene, idx) {
   boss.status = "순찰";
   scene.board.step = "exploreRun";
   const nodes = rollPatrolEvents(idx, scene);
-  scene.board.exploreRun = { idx, boss, event: 0, line: 0, timer: 0, steps: 0, maxSteps: nodes.maxSteps || 32, nodes, shown: [`${boss.name}이 ${area.name} 순찰을 시작했다.`], rewards: { soul: 0, notoriety: 0, monsters: 0, clues: 0, exp: 0, levelUps: 0 }, hp: boss.hp, maxHp: boss.maxHp, finished: false };
+  scene.board.exploreRun = { idx, boss, event: 0, line: 0, timer: 0, steps: 0, maxSteps: nodes.maxSteps || 64, nodes, shown: [`${boss.name}이 ${area.name} 순찰을 시작했다.`], rewards: { soul: 0, notoriety: 0, monsters: 0, clues: 0, exp: 0, levelUps: 0 }, hp: boss.hp, maxHp: boss.maxHp, finished: false };
   scene.board.typewriter = { full: scene.board.exploreRun.shown.join("\n"), chars: 0 };
   scene.board.logs.unshift(`${boss.name}이 ${area.name} 순찰을 시작했다.`);
   renderBoard(scene);
@@ -2158,7 +2158,7 @@ function openComppTour(scene) {
   const run = scene.board?.exploreRun;
   const dungeonNo = run ? scene.board.areas[run.idx]?.dungeonNo || 1 : 1;
   const boss = run?.boss;
-  const qs = new URLSearchParams({ v: "20260826ax", dungeon: dungeonNo, boss: boss?.name || "순찰자", hp: boss?.hp || 90, maxHp: boss?.maxHp || 90, atk: boss?.atk || 18, def: boss?.def || 0, maxSteps: run?.maxSteps || 32 });
+  const qs = new URLSearchParams({ v: "20260826ay", dungeon: dungeonNo, boss: boss?.name || "순찰자", hp: boss?.hp || 90, maxHp: boss?.maxHp || 90, atk: boss?.atk || 18, def: boss?.def || 0, maxSteps: run?.maxSteps || 64 });
   frame.src = `new_/patrol_tour.html?${qs.toString()}`;
   frame.title = "BOARD-TOUR patrol";
   Object.assign(frame.style, {
